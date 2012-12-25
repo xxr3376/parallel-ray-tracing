797006d83c7bf616bf06db4e4db5e9e61d9f1327
UGeeker <ugeeker@ugeeker-ThinkPad-X201-Tablet.(none)>
2012-12-25 23:45:16 +0800

    make sure I can use CV in main function, Still have problem when both use cilk & opencv, low g++ version cause link error in cv, TAT.

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
 #include <cstdio>
 #include <vector>
 #include "pnt.h"
 #include "triangle.h"
 #include "common.h"
 
-using std::vector;
+using namespace cv;
+using namespace std;
 
+//int cilk_main() {
 int main() {
-	
+
 	vector<Triangle> triangles;
 	read_obj_file("../data/Sphere01.obj", triangles);
 
@@ -15,6 +20,13 @@ int main() {
 		Triangle &t = triangles[i];
 		printf("(%f,%f,%f) , (%f,%f,%f) , (%f,%f,%f) : (%f,%f,%f)\n", t.a.x, t.a.y, t.a.z, t.b.x, t.b.y, t.b.z, t.c.x, t.c.y, t.c.z, t.x, t.y, t.z);
 	}
+
+	Mat img = imread("lena.jpg"); 
+	if(!img.data) 
+		return -1; 
+	namedWindow("Lena", CV_WINDOW_AUTOSIZE); 
+	imshow("Lena", img); 
+	waitKey();
 
 	return 0;
 }
