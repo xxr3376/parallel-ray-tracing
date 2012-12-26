b6e435834ed00b5cd8582132fced0d03735263fa
UGeeker <ugeeker@ugeeker-ThinkPad-X201-Tablet.(none)>
2012-12-26 08:38:03 +0800

    a try of testing judgement instect

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "pnt.h"
 #include "triangle.h"
 #include "common.h"
+#include "line.h"
+#include "bvhtree.h"
 
 using namespace cv;
 using namespace std;
@@ -16,17 +18,44 @@ int main() {
 	vector<Triangle> triangles;
 	read_obj_file("../data/Sphere01.obj", triangles);
 
-	for (int i = 0; i < triangles.size(); ++i) {
-		Triangle &t = triangles[i];
-		printf("(%f,%f,%f) , (%f,%f,%f) , (%f,%f,%f) : (%f,%f,%f)\n", t.a.x, t.a.y, t.a.z, t.b.x, t.b.y, t.b.z, t.c.x, t.c.y, t.c.z, t.x, t.y, t.z);
-	}
+	BVHTree bvh;
+	printf("begin to build tree");
+	bvh.create_tree(triangles.size(), &triangles[0]);
+	printf("build success");
 
-	Mat img = imread("lena.jpg"); 
-	if(!img.data) 
-		return -1; 
-	namedWindow("Lena", CV_WINDOW_AUTOSIZE); 
-	imshow("Lena", img); 
-	waitKey();
+	int WIDTH = 800, HEIGHT = 600;
+	IplImage *im = cvCreateImage(cvSize(WIDTH, HEIGHT), 8, 3);//创建一个图像
+	for (int i = 0; i < WIDTH; i++)
+	{
+		//遍历每行
+		for (int j = 0; j < HEIGHT; j ++)
+		{
+			//遍历每列
+			//im->imageData[j*im->widthStep+3*i+2] = (int) r.r;
+			//im->imageData[j*im->widthStep+3*i+1] = (int) r.g;
+			//im->imageData[j*im->widthStep+3*i+0] = (int) r.b;
+			int x = i + WIDTH / 2;
+			int y = j + HEIGHT / 2;
+			printf("(%d, %d)\n", x, y);
+			int index = 0;
+			Vector3 point;
+			Line3 input(Vector3(x, y, 0), Vector3(0, 0 ,1));
+			int k = bvh.intersect(input, point, index) * 255;
+			im->imageData[j*im->widthStep+3*i+2] = k;
+            im->imageData[j*im->widthStep+3*i+1] = k;
+            im->imageData[j*im->widthStep+3*i+0] = k;
+		}
+	}
+	cvShowImage("rayTracing", im);
+	waitKey(0);
+
+
+	//Mat img = imread("lena.jpg"); 
+	//if(!img.data) 
+	//	return -1; 
+	//namedWindow("Lena", CV_WINDOW_AUTOSIZE); 
+	//imshow("Lena", img); 
+	//waitKey();
 
 	return 0;
 }
